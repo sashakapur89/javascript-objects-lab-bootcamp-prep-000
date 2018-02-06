a431c63f5fb49db702124453c56980e55875ae7b
Sasha Kapur <sashakapur89@gmail.com>
2018-02-06 21:23:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,4 +15,7 @@ function deleteFromObjectByKey(object, key){
   return newObject;
 }
 
-function destructivelyUpdateObjectWithKeyAndValue
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object.key;
+  return object;
+}
